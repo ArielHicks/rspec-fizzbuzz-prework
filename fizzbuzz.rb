a8f48265feff604b07aaf027f858f942aa00fe62
Ariel Hicks <hicks.ariel@gmail.com>
2018-12-19 21:18:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,9 @@
 #We expect fizzbuzz(3) to return "Fizz"
 describe "fizzbuzz" do
-  it 'returns "Fizz" when the enumber is divisible by 3' do
+  it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
+ 
+    expect(fizz_3).to eq("Fizz")
   end
 end
 #We expect fizzbuzz(5) to return "Buzz"
