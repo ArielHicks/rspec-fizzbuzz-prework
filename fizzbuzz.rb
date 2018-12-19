1cd87223963298ad84e66890d599d0070bbbab87
Ariel Hicks <hicks.ariel@gmail.com>
2018-12-19 21:17:16 +0000

    Automatically backed up by Learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,9 @@
+#We expect fizzbuzz(3) to return "Fizz"
+describe "fizzbuzz" do
+  it 'returns "Fizz" when th enumber is divisible by 3' do
+    fizz_3 = fizzbuzz(3)
+  end
+end
+#We expect fizzbuzz(5) to return "Buzz"
+#We expect fizzbuzz(15) to return "FizzBuzz"
+#We expect fizzbuzz(4) to return nil
